69c085d2e3f37359c53a6cbb6ac60f1c9cdde3b1
Kelvin Wu <wukuwnyu@gmail.com>
2020-01-10 18:10:50 +0800

    dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,75 @@
 import React, { useState } from 'react';
 
+interface Checkbox {
+	name: string;
+	label: string
+	key: string;
+}
+
+interface FormData {
+	
+}
 const App: React.FC = () => {
-	const checkBoxList = [
-		"Anaual Leave",
-		"Sick Leave",
-		"Unpaid Leave",
+	const [values, setValues] = useState<any>({
+		"employeeName": "",
+		"employeeEmail": "",
+		"anaualLeave": false,
+		"sickLeave": false,
+		"unpaidLeave": false,
+		"othersLeave": false,
+		"othersReasons": "",
+		"dateFrom": "",
+		"dateTo": "",
+		"managerName": "",
+		"managerEmail": ""
+	});
+
+	const handleChange = (e: any) => {
+
+		setValues({
+			...values,
+			[e.target.name]: e.target.value
+		});
+		console.log(values);
+	}
+
+	const handleClick = (e: any) => {
+
+		setValues({
+			...values,
+			[e.target.name]: e.target.checked
+		});
+		console.log(values);
+	}
+
+	const handleSubmit = (e: any) => {
+		e.preventDefault();
+		const data: FormData = {
+
+		}
+	}
+
+	const checkboxList: Checkbox[] = [
+		{
+			name: "anaualLeave",
+			label: "Anaual Leave",
+			key: "0"
+		},
+		{
+			name: "sickLeave",
+			label: "Sick Leave",
+			key: "1"
+		},
+		{
+			name: "unpaidLeave",
+			label: "Unpaid Leave",
+			key: "2"
+		},
+		{
+			name: "othersLeave",
+			label: "Others",
+			key: "3"
+		}
 	];
 
 	return (
@@ -14,46 +79,42 @@ const App: React.FC = () => {
 				<label>Requested Employee Information</label>
 				<div>
 					<label>Name</label>
-					<input name="name" type="text" />
+					<input name="employeeName" type="text" onChange={handleChange} />
 					<label>Email</label>
-					<input name="email" type="text" />
+					<input name="employeeEmail" type="text" onChange={handleChange} />
 				</div>
 				<div>
 					<label>Reason for Requested Leave</label>
 					<div>
 						{
-							checkBoxList.map(s =>
-								<div>
-									<input type="checkbox" />
-									<label>{s}</label>
+							checkboxList.map(item =>
+								<div key={item.key}>
+									<input name={item.name} type="checkbox" checked={values[item.name]} onClick={handleClick} />
+									<label>{item.label}</label>
 								</div>
 							)
 						}
-						<div>
-							<input type="checkbox" />
-							<label>Other</label>
-							{}
-						</div>
 					</div>
 				</div>
 				<div>
 					<label>Date Requested</label>
 					<div>
 						<label>From</label>
-						<input type="text" name="date-from" />
+						<input type="text" name="dateFrom" onChange={handleChange} />
 						<label>To</label>
-						<input type="date" name="date-to" />
+						<input type="date" name="dateTo" onChange={handleChange} />
 					</div>
 				</div>
 				<div>
 					<label>Manager Information for Approval</label>
 					<div>
 						<label>Name</label>
-						<input type="text" name="date-from" />
+						<input type="text" name="managerName" onChange={handleChange} />
 						<label>Email</label>
-						<input type="date" name="date-to" />
+						<input type="date" name="managerEmail" onChange={handleChange} />
 					</div>
 				</div>
+				<button onSubmit={handleSubmit}>Submit</button>
 			</form>
 		</div>
 	);
